394038adb0f13a2b39e2d6b0de159815cd66aaae
Sreesh Maheshwar <sreesh.maheshwar21@imperial.ac.uk>
2024-02-17 18:13:17 +0000

    Index ratings on username field

--- app/api/puzzle/submit/RatingColl.ts
+++ app/api/puzzle/submit/RatingColl.ts
@@ -1,12 +1,15 @@
 import mongoose, { Schema } from 'mongoose';
 
 const ratingSchema = new Schema({
-  username: String,
+  username: { type: String, required: true },
   rating: Number,
   ratingDeviation: Number,
   volatility: Number,
   numberOfResults: Number,
 });
+
+// Index on username for fast lookups.
+ratingSchema.index({ username: 1 }, { unique: true });
 
 export const RatingColl =
   mongoose.models.Rating || mongoose.model('Rating', ratingSchema);
