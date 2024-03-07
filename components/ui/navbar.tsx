15586152aa9c5660b5b452bf28d03607a8c0afa5
yichenchong <yichenchong@yahoo.com>
2024-03-07 08:46:49 +0000

    Added nav to navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import Link from 'next/link';
 import AuthButton from '@/components/auth/auth-button';
 import ModeToggler from './mode-toggle';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 
 type NavbarProps = {
   user: any;
@@ -17,9 +19,31 @@ export default function Navbar({ user }: NavbarProps) {
           Lychee_
         </Link>
       </div>
+      <div className='m-8 text-sm underline font-mono block sm:hidden'><a href='/trainer'>Trainer</a></div>
       <div className='flex-grow'></div>
-      <AuthButton user={user} className='' />
-      <ModeToggler className='' />
+      <AuthButton user={user} className='block sm:hidden' />
+      <ModeToggler className='block sm:hidden' />
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild className='hidden sm:block'>
+          <button className="IconButton" aria-label="Menu">
+            <HamburgerMenuIcon />
+          </button>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+            <DropdownMenu.Item className="bg-background hover:bg-foreground hover:text-background">
+              <Link href='/trainer'>Trainer</Link>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className="bg-background">
+              <AuthButton user={user} className='' />
+            </DropdownMenu.Item>
+            <DropdownMenu.Item className="bg-background">
+              <ModeToggler className='' />
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
     </nav>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      screens: {
+        'sm': {'max': '768px'}
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
