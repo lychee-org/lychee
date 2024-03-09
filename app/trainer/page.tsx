4b85fc447c038627837604a036667344f8c5d63d
Sreesh Maheshwar <sreesh.maheshwar21@imperial.ac.uk>
2024-03-09 22:39:06 +0000

    Fix: redirect in trainer if not logged in

--- app/trainer/page.tsx
+++ app/trainer/page.tsx
@@ -3,11 +3,14 @@ import PuzzleMode from '@/components/puzzle-ui/puzzle-mode';
 import React from 'react';
 import { validateRequest } from '@/lib/auth';
 import nextPuzzleFor from '../api/puzzle/nextPuzzle/nextFor';
+import { redirect } from 'next/navigation';
 
 export default async function TestBoard() {
   await dbConnect();
   const { user } = await validateRequest();
-  if (!user) return new Response('Unauthorized', { status: 401 });
+  if (!user) {
+    return redirect('/');
+  }
   const { puzzle, rating } = await nextPuzzleFor(user);
   return (
     <PuzzleMode initialPuzzle={puzzle} initialRating={rating} group={[]} />
