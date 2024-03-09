1d25450324ef6bedb1d86a39ce9ca777904c278c
ahmedmahmud <ahmedmahmud12971@gmail.com>
2024-03-09 21:18:54 +0000

    chore: prettier

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Dashboard from './(dashboard)/dashboard';
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  
   const { user } = await validateRequest();
   if (!user) {
     return <Unauthorized />;
