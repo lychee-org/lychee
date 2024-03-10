4301caaa12c6b45abfb04942f8691fc130759fa7
ahmedmahmud <ahmedmahmud12971@gmail.com>
2024-03-10 18:26:25 +0000

    feat: add scrollbar to grouos

--- app/(dashboard)/themes-list.tsx
+++ app/(dashboard)/themes-list.tsx
@@ -10,10 +10,7 @@ import type { ThemeData } from '../api/dashboard/getThemes';
 import Delta from '@/components/ui/delta';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
-import { set } from 'mongoose';
 import { Separator } from '@/components/ui/separator';
-import { Skeleton } from '@/components/ui/skeleton';
-import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
 const PROV_DEVIATION: number = 110;
